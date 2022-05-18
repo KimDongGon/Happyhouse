ec1becd10ef48008ac8a0cd8055a62df9fb6b2ab
ehdrhs329 <ehdrhs329@gmail.com>
2022-05-19 01:07:43 +0900

    Modify HouseSearch name

--- front/src/components/HouseSearch.vue
+++ front/src/components/HouseSearch.vue
@@ -22,6 +22,7 @@
           <b-form-select
             v-model="dongCode"
             :options="dong"
+            @change="dongChanged"
             class="bg-dark text-light"
           ></b-form-select>
         </b-col>
@@ -35,7 +36,7 @@
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapState, mapActions, mapMutations } from "vuex";
 export default {
   data() {
     return {
@@ -48,24 +49,55 @@ export default {
     ...mapState(["sido", "gugun", "dong"]),
   },
   created() {
+    this.sidoCode = this.$store.state.sidoCode;
+    this.gugunCode = this.$store.state.gugunCode;
+    this.dongCode = this.$store.state.dongCode;
     this.getSido();
   },
   methods: {
     ...mapActions(["getSido", "getGugun", "getDong"]),
+    ...mapMutations(["SET_SIDO_CODE", "SET_GUGUN_CODE", "SET_DONG_CODE"]),
     sidoChanged() {
+      this.SET_SIDO_CODE(this.sidoCode);
+
       this.gugunCode = null;
       this.dongCode = null;
+      this.SET_GUGUN_CODE(null);
+      this.SET_DONG_CODE(null);
       if (this.sidoCode !== null) {
         this.getGugun(this.sidoCode);
       }
     },
     gugunChanged() {
+      this.SET_GUGUN_CODE(this.gugunCode);
+
       this.dongCode = null;
+      this.SET_DONG_CODE(null);
       if (this.gugunCode !== null) {
         this.getDong({ sidoCode: this.sidoCode, gugunCode: this.gugunCode });
       }
     },
-    search() {},
+    dongChanged() {
+      this.SET_DONG_CODE(this.dongCode);
+    },
+    search() {
+      if (this.sidoCode === null) {
+        alert("시/도를 선택해주세요.");
+      } else if (this.gugunCode === null) {
+        alert("구/군을 선택해주세요.");
+      } else if (this.dongCode === null) {
+        alert("동을 선택해주세요.");
+      } else {
+        this.$router.push({
+          name: "search",
+          params: {
+            sidoCode: this.sidoCode,
+            gugunCode: this.gugunCode,
+            dongCode: this.dongCode,
+          },
+        });
+      }
+    },
   },
 };
 </script>
