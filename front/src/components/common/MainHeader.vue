9ec48cfd2b3aa956db171e3f5641c70efe919e74
ehdrhs329 <ehdrhs329@gmail.com>
2022-05-18 11:00:18 +0900

    Add route userlist

---
+++ front/src/components/common/MainHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <div>signup</div>
+    <div>login</div>
+    <div><img /></div>
+    <div>공지사항</div>
+    <div>오늘의 뉴스</div>
+    <div>Profile</div>
+    <div><router-link to="/admin">UserList</router-link></div>
+    <div>logout</div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
