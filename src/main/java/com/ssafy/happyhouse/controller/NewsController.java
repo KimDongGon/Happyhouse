018abe637a79b194141c90a99c2c811075c04433
김혜진 <aa981204@naver.com>
2022-05-24 23:26:20 +0900

    Add AptArticle

--- src/main/java/com/ssafy/happyhouse/controller/NewsController.java
+++ src/main/java/com/ssafy/happyhouse/controller/NewsController.java
@@ -31,59 +31,6 @@ import java.text.*;
 public class NewsController {
 	private static final Logger logger = LoggerFactory.getLogger(NewsController.class);
 	
-	public static HashMap<String, String> map;
-	
-//	@GetMapping
-//	public ResponseEntity<Map<Integer, NewsDto>> startCrawl() throws IOException { 		
-//
-//		Map<Integer, NewsDto> map = new HashMap<Integer, NewsDto>();
-//		SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd", Locale.KOREA);
-//		Date currentTime = new Date();
-//		
-//		String dTime = formatter.format(currentTime);
-//		String e_date = dTime;
-//		
-//		currentTime.setDate(currentTime.getDate() - 1);
-//		String s_date = formatter.format(currentTime);
-//		
-//		String query = "사회";
-//		String s_from = s_date.replace(".", "");
-//		String e_to = e_date.replace(".", "");
-//		
-//		int page = 1;
-//		ArrayList<String> al1 = new ArrayList<>();
-//		ArrayList<String> al2 = new ArrayList<>();
-//		
-//		while(page < 20) {
-//			String address = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort=1&ds=" + s_date
-//					+ "&de=" + e_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start="
-//					+ Integer.toString(page);
-//			
-//			Document rawData = Jsoup.connect(address).timeout(5000).get();
-//			System.out.println(address);
-//			Elements blogOption = rawData.select("dl dt");
-//			String realURL = "";
-//			String realTITLE = "";
-//			int i=0;
-//			for(Element option : blogOption) {
-//				realURL = option.select("a").attr("href");
-//				System.out.println("URL >>>>>> " + realURL);
-//				realTITLE = option.select("a").attr("title");
-//				System.out.println("TITLE >>>>>> " + realTITLE);
-//				NewsDto news = new NewsDto();
-//				
-//				news.setUrl(realURL);
-//				news.setTitle(realTITLE);
-//				
-//				map.put(i,news);
-//				i++;
-//			}
-//			page += 10;
-//		}
-//		
-//		return new ResponseEntity<Map<Integer,NewsDto>>(map, HttpStatus.OK);
-//	}
-	
 	@GetMapping
 	public ResponseEntity<List<NewsDto>> startCrawl() throws IOException { 		
 
@@ -110,6 +57,57 @@ public class NewsController {
 				+ Integer.toString(page);
 		
 		Document rawData = Jsoup.connect(address).timeout(5000).get();
+		//System.out.println("주소 >>> " + address);
+		Elements blogOption = rawData.select(".news_tit");
+		
+		String realURL = "";
+		String realTITLE = "";
+		
+		int index = 0;
+		for(Element option : blogOption) {
+			realURL = option.attr("href");
+			//System.out.println("realURL >>> " + realURL);
+			realTITLE = option.attr("title");
+			//System.out.println("realTitle >>> " + realTITLE);
+			NewsDto news = new NewsDto();
+			
+			news.setUrl(realURL);
+			news.setTitle(realTITLE);
+			
+			list.add(news);
+			index++;
+			if(index>4) break;
+		}
+		
+		return new ResponseEntity<List<NewsDto>>(list, HttpStatus.OK);
+	}
+	
+	@GetMapping("/apt")
+	public ResponseEntity<List<NewsDto>> startAptCrawl() throws IOException { 		
+
+		List<NewsDto> list = new ArrayList();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd", Locale.KOREA);
+		Date currentTime = new Date();
+		
+		String dTime = formatter.format(currentTime);
+		String e_date = dTime;
+		
+		currentTime.setDate(currentTime.getDate() - 1);
+		String s_date = formatter.format(currentTime);
+		
+		String query = "아파트매매";
+		String s_from = s_date.replace(".", "");
+		String e_to = e_date.replace(".", "");
+		
+		int page = 1;
+		ArrayList<String> al1 = new ArrayList<>();
+		ArrayList<String> al2 = new ArrayList<>();
+		
+		String address = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort=1&ds=" + s_date
+				+ "&de=" + e_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start="
+				+ Integer.toString(page);
+		
+		Document rawData = Jsoup.connect(address).timeout(5000).get();
 		System.out.println("주소 >>> " + address);
 		Elements blogOption = rawData.select(".news_tit");
 		//String text = blogOption.text();
@@ -118,17 +116,20 @@ public class NewsController {
 		String realURL = "";
 		String realTITLE = "";
 		
+		int index = 0;
 		for(Element option : blogOption) {
 			realURL = option.attr("href");
-			System.out.println("realURL >>> " + realURL);
+			//System.out.println("realURL >>> " + realURL);
 			realTITLE = option.attr("title");
-			System.out.println("realTitle >>> " + realTITLE);
+			//System.out.println("realTitle >>> " + realTITLE);
 			NewsDto news = new NewsDto();
 			
 			news.setUrl(realURL);
 			news.setTitle(realTITLE);
 			
 			list.add(news);
+			index++;
+			if(index>4) break;
 		}
 		
 		return new ResponseEntity<List<NewsDto>>(list, HttpStatus.OK);
