0775ad8cb8f164ec04834382b48e48ca240b3975
ehdrhs329 <ehdrhs329@gmail.com>
2022-05-26 13:47:57 +0900

    Add address api in sign up
    Former-commit-id: 618d517c4ec71a630d75b3be29b3d6527a3cf7b8

--- front/src/components/user/SignUpForm.vue
+++ front/src/components/user/SignUpForm.vue
@@ -105,9 +105,30 @@
             v-model="$v.form.userAddress.$model"
             :state="validateState('userAddress')"
             aria-describedby="addressFeedback"
+            readonly
+            @click="showAddress"
           />
           <b-form-invalid-feedback id="addressFeedback"
             >주소는 5자이상 45자이하여야 합니다.</b-form-invalid-feedback
+          >
+        </b-form-group>
+
+        <b-form-group
+          label="상세주소"
+          label-size="lg"
+          label-for="userDetailAddress"
+          class="mt-1"
+        >
+          <b-form-input
+            type="text"
+            class="form-control-lg rounded-0"
+            placeholder="상세주소"
+            v-model="$v.form.userDetailAddress.$model"
+            :state="validateState('userDetailAddress')"
+            aria-describedby="detailAddressFeedback"
+          />
+          <b-form-invalid-feedback id="detailAddressFeedback"
+            >상세 주소는 입력되어야 합니다.</b-form-invalid-feedback
           >
         </b-form-group>
 
@@ -165,6 +186,7 @@ export default {
         userAddress: null,
         userMobile: null,
       },
+      isShow: false,
     };
   },
   validations: {
@@ -208,6 +230,10 @@ export default {
         minLength: minLength(5),
         maxLength: maxLength(45),
       },
+      userDetailAddress: {
+        required,
+        minLength: minLength(1),
+      },
       userMobile: {
         required,
         minLength: minLength(13),
@@ -240,6 +266,7 @@ export default {
           // eslint-disable-next-line prettier/prettier
           this.form.userPasswordCheck
         ),
+        userAddress: `${this.form.userAddress}, ${this.form.userDetailAddress}`,
       };
       this.signUp(userInfo);
     },
@@ -249,6 +276,47 @@ export default {
         // eslint-disable-next-line prettier/prettier
         "$1-$2-$3"
       );
+    },
+    showAddress() {
+      if (this.isShow) return;
+      this.isShow = true;
+      new window.daum.Postcode({
+        oncomplete: (data) => {
+          // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+          // 도로명 주소의 노출 규칙에 따라 주소를 조합한다.
+          // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+          let fullRoadAddr = data.roadAddress; // 도로명 주소 변수
+          let extraRoadAddr = ""; // 도로명 조합형 주소 변수
+
+          // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+          // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+          if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+            extraRoadAddr += data.bname;
+          }
+          // 건물명이 있고, 공동주택일 경우 추가한다.
+          if (data.buildingName !== "" && data.apartment === "Y") {
+            extraRoadAddr +=
+              extraRoadAddr !== ""
+                ? ", " + data.buildingName
+                : data.buildingName;
+          }
+          // 도로명, 지번 조합형 주소가 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+          if (extraRoadAddr !== "") {
+            extraRoadAddr = " (" + extraRoadAddr + ")";
+          }
+          // 도로명, 지번 주소의 유무에 따라 해당 조합형 주소를 추가한다.
+          if (fullRoadAddr !== "") {
+            fullRoadAddr += extraRoadAddr;
+          }
+
+          // 우편번호와 주소 정보를 해당 필드에 넣는다.
+          this.form.userAddress = `(${data.zonecode}) ${fullRoadAddr}`;
+        },
+        onclose: () => {
+          this.isShow = false;
+        },
+      }).open();
     },
   },
 };
